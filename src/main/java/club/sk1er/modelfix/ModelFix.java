a582bd1511007f248630681d765d700a8a2f0a7e
asbyth <cyronize@gmail.com>
2020-08-05 12:21:48 -0400

    fix: actually initialize modcore

--- src/main/java/club/sk1er/modelfix/ModelFix.java
+++ src/main/java/club/sk1er/modelfix/ModelFix.java
@@ -1,6 +1,9 @@
 package club.sk1er.modelfix;
 
+import club.sk1er.modcore.ModCoreInstaller;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(
         modid = ModelFix.MOD_ID,
@@ -11,4 +14,9 @@ public class ModelFix {
     public static final String MOD_ID = "modelfix";
     public static final String MOD_NAME = "ModelFix";
     public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        ModCoreInstaller.initializeModCore(Minecraft.getMinecraft().mcDataDir);
+    }
 }
